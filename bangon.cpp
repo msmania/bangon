9e92f7e1ca9d9344fb29234d96306b8736085b75
tokikuch <k.toshihito@yahoo.de>
2015-02-15 10:46:50 -0800

    added 32bit support

--- bangon.cpp
+++ bangon.cpp
@@ -2,14 +2,17 @@
 // bangon.cpp
 //
 
+// IsPtr64() is supported only in 64bit pointer mode.
+#define KDEXT_64BIT
+
 #include <Windows.h>
 #include <set>
 #include <queue>
 
 #include "wdbgexts.h"
 
-#define LODWORD(ll) ((DWORD)(ll&0xffffffff))
-#define HIDWORD(ll) ((DWORD)((ll>>32)&0xffffffff))
+#define LODWORD(ll) ((DWORD)((ll)&0xffffffff))
+#define HIDWORD(ll) ((DWORD)(((ll)>>32)&0xffffffff))
 
 //
 // http://msdn.microsoft.com/en-us/library/windows/hardware/ff543968(v=vs.85).aspx
@@ -41,38 +44,75 @@ VOID WinDbgExtensionDllInit(
 }
 
 DECLARE_API (help) {
-    dprintf("Hello!\n");
+    dprintf("!dt <RTL_SPLAY_LINKS*>                         - dump splay tree\n\n");
 }
 
+struct TREE_ITEM32 {
+    ULONG Parent;
+    ULONG LeftChild;
+    ULONG RightChild;
+};
+
 struct TREE_ITEM64 {
-    ULONGLONG Parent;
-    ULONGLONG LeftChild;
-    ULONGLONG RightChild;
+    ULONG64 Parent;
+    ULONG64 LeftChild;
+    ULONG64 RightChild;
 };
 
 struct TREE_ITEM_INFO {
     DWORD Level;
-    ULONGLONG Myself;
-    TREE_ITEM64 Item;
+    ULONG64 Myself;
+    union {
+        TREE_ITEM32 Item32;
+        TREE_ITEM64 Item64;
+    } u;
+
+    ULONG64 Parent() const {
+        return IsPtr64() ? u.Item64.Parent : u.Item32.Parent;
+    }
+
+    ULONG64 LeftChild() const {
+        return IsPtr64() ? u.Item64.LeftChild : u.Item32.LeftChild;
+    }
+
+    ULONG64 RightChild() const {
+        return IsPtr64() ? u.Item64.RightChild : u.Item32.RightChild;
+    }
 };
 
 std::queue<TREE_ITEM_INFO> TraverseQueue;
-std::set<ULONGLONG> CorruptionCheck;
+std::set<ULONG64> CorruptionCheck;
 
-BOOL AddTreeItem(DWORD Level, ULONGLONG Address) {
+static LPCSTR ptos(ULONG64 p, LPSTR s, ULONG len) {
+    LPCSTR Ret = NULL;
+    if ( HIDWORD(p)==0 && len>=9 ) {
+        sprintf_s(s, len, "%08x", LODWORD(p));
+        Ret = s;
+    }
+    else if ( HIDWORD(p)>0 && len>=18 ) {
+        sprintf_s(s, len, "%08x`%08x", HIDWORD(p), LODWORD(p));
+        Ret = s;
+    }
+    return Ret;
+}
+
+BOOL AddTreeItem(DWORD Level, ULONG64 Address) {
     if ( CorruptionCheck.find(Address)!=CorruptionCheck.end() ) {
         return FALSE;
     }
 
     CorruptionCheck.insert(Address);
 
+    ULONG Status = 0;
     DWORD BytesRead = 0;
+    DWORD BytesToRead = 0;
+
     TREE_ITEM_INFO TreeItem;
     TreeItem.Level = Level;
     TreeItem.Myself = Address;
-    ReadMemory(Address, &(TreeItem.Item), sizeof(TREE_ITEM64), &BytesRead);
-
-    if ( BytesRead!=sizeof(TREE_ITEM64) ) {
+    BytesToRead = IsPtr64() ? sizeof(TREE_ITEM64) : sizeof(TREE_ITEM32);
+    Status = ReadMemory(Address, &(TreeItem.u), BytesToRead, &BytesRead);
+    if ( !Status || BytesRead!=BytesToRead ) {
         return FALSE;
     }
 
@@ -80,12 +120,22 @@ BOOL AddTreeItem(DWORD Level, ULONGLONG Address) {
     return TRUE;
 }
 
-DECLARE_API (dumptree) {
-    ULONGLONG RootAddress = GetExpression(args);
+DECLARE_API (dt) {
+    ULONG64 RootAddress = 0;
+    CHAR buf1[20];
+    CHAR buf2[20];
+    CHAR buf3[20];
+    CHAR buf4[20];
 
+    RootAddress = GetExpression(args);
     if ( !RootAddress )
         return;
-    
+
+    if ( !IsPtr64() ) {
+        // preventing sign extension
+        RootAddress &= 0x0ffffffff;
+    }
+
     while ( TraverseQueue.size() ) {
         TraverseQueue.pop();
     }
@@ -98,29 +148,29 @@ DECLARE_API (dumptree) {
     while ( TraverseQueue.size() ) {
         const TREE_ITEM_INFO &Item = TraverseQueue.front();
 
-        dprintf("L=%04x#%04x %08x`%08x : P= %08x`%08x L= %08x`%08x R= %08x`%08x\n",
+        dprintf("L=%04x#%04x %s : P= %s L= %s R= %s\n",
             CurrentLevel, ItemCount,
-            HIDWORD(Item.Myself), LODWORD(Item.Myself),
-            HIDWORD(Item.Item.Parent), LODWORD(Item.Item.Parent),
-            HIDWORD(Item.Item.LeftChild), LODWORD(Item.Item.LeftChild),
-            HIDWORD(Item.Item.RightChild), LODWORD(Item.Item.RightChild));
+            ptos(Item.Myself, buf1, sizeof(buf1)),
+            ptos(Item.Parent(), buf2, sizeof(buf2)),
+            ptos(Item.LeftChild(), buf3, sizeof(buf3)),
+            ptos(Item.RightChild(), buf4, sizeof(buf4)));
         
         if ( Item.Level!=CurrentLevel ) {
             ItemCount = 0;
             CurrentLevel = Item.Level;
         }
         
-        if ( Item.Item.LeftChild ) {
-            if ( !AddTreeItem(Item.Level+1, Item.Item.LeftChild) ) {
-                dprintf("Item %08x`%08x was duplicated!\n",
-                    HIDWORD(Item.Item.LeftChild), LODWORD(Item.Item.LeftChild));
+        if ( Item.LeftChild() ) {
+            if ( !AddTreeItem(Item.Level+1, Item.LeftChild()) ) {
+                dprintf("Item %s was duplicated!\n",
+                    ptos(Item.LeftChild(), buf1, sizeof(buf1)));
             }
         }
 
-        if ( Item.Item.RightChild ) {
-            if ( !AddTreeItem(Item.Level+1, Item.Item.RightChild) ) {
-                dprintf("Item %08x`%08x was duplicated!\n",
-                    HIDWORD(Item.Item.RightChild), LODWORD(Item.Item.RightChild));
+        if ( Item.RightChild() ) {
+            if ( !AddTreeItem(Item.Level+1, Item.RightChild()) ) {
+                dprintf("Item %s was duplicated!\n",
+                    ptos(Item.RightChild(), buf1, sizeof(buf1)));
             }
         }
 
